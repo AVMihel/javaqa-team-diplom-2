7880558f4da49d40dacb95dfeb60f6fe2cd14415
Dimmidroll <diman15061997@mail.ru>
2025-03-19 19:46:23 +0500

    New tests for Bank added

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -2,6 +2,9 @@ package ru.netology.javaqadiplom;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BankTest {
@@ -236,6 +239,116 @@ public class BankTest {
         Assertions.assertAll(
                 () -> assertEquals(7_000, saving.getBalance()),
                 () -> assertEquals(0, credit.getBalance()),
+                () -> assertFalse(result)
+        );
+    }
+
+    @Test
+    public void shouldTransferFromSavingToSaving() {
+        Account saving1 = new SavingAccount(
+                5_000,
+                0_000,
+                10_000,
+                5
+        );
+
+        Account saving2 = new SavingAccount(
+                0,
+                0,
+                10_000,
+                5
+        );
+
+        Bank bank = new Bank();
+
+        boolean result = bank.transfer(saving1, saving2, 5_000);
+
+        Assertions.assertAll(
+                () -> assertEquals(0, saving1.getBalance()),
+                () -> assertEquals(5_000, saving2.getBalance()),
+                () -> assertTrue(result)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 6000",
+            "2000,1000"
+    })
+    public void shouldNotTransferFromSavingToSavingNegativeTests(int minBalance1, int initialBalance2) {
+        Account saving1 = new SavingAccount(
+                6_000,
+                minBalance1,
+                10_000,
+                5
+        );
+
+        Account saving2 = new SavingAccount(
+                initialBalance2,
+                0,
+                10_000,
+                5
+        );
+
+        Bank bank = new Bank();
+
+        boolean result = bank.transfer(saving1, saving2, 5_000);
+
+        Assertions.assertAll(
+                () -> assertEquals(6_000, saving1.getBalance()),
+                () -> assertEquals(initialBalance2, saving2.getBalance()),
+                () -> assertFalse(result)
+        );
+    }
+
+    @Test
+    public void shouldTransferFromCreditToCredit() {
+        Account credit1 = new CreditAccount(
+                1_000,
+                2_000,
+                15
+        );
+
+        Account credit2 = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Bank bank = new Bank();
+
+        boolean result = bank.transfer(credit1, credit2, 1_000);
+
+        Assertions.assertAll(
+                () -> assertEquals(0, credit1.getBalance()),
+                () -> assertEquals(1_000, credit2.getBalance()),
+                () -> assertTrue(result)
+        );
+    }
+
+    @Test
+    public void shouldNotTransferFromCreditToCreditResultBalanceLessCreditLimit() {
+        Account credit1 = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Account credit2 = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Bank bank = new Bank();
+
+        credit1.pay(4_000);
+
+        boolean result = bank.transfer(credit1, credit2, 2_000);
+
+        Assertions.assertAll(
+                () -> assertEquals(-4_000, credit1.getBalance()),
+                () -> assertEquals(0, credit2.getBalance()),
                 () -> assertFalse(result)
         );
     }
